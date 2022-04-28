8a60897a35aad2a89781eb0f59c13c3753fb06d2
Profesoor <ariel3333876@gmail.com>
2022-04-28 17:47:17 +0300

    finished python script

--- trivia_script_tester.py
+++ trivia_script_tester.py
@@ -4,25 +4,43 @@ import json
 IP = "127.0.0.1"
 PORT = 42069
 
-def get_data_from_socket(sock):
-    return sock.recv(1024).decode()
+SIGN_UP = '1'
+LOGIN = '5'
 
-def get_code_from_socket(sock):
-    return sock.recv(1).decode()
+def get_data_from_socket(sock, byte_num):
+    return sock.recv(byte_num).decode()
 
-def send_data_to_server(sock, data):
-    msg = "100"
-    msg += ( str(len(json.dumps(data))) + json.dumps(data) )
-    msg = data
+def get_string_data(sock):
+    code = str(sock.recv(1).decode())
+    data = get_data_from_socket(sock, sock.recv(4).decode())
+    return f"CODE:{code}, DATA:{data}"
+
+def send_request(sock, json_data, request_code):
+    msg += request_code
+    msg += str(len(json_data))
+    msg += json_data
     sock.send(msg.encode())
-
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((IP, PORT))
 
-    print(get_data_from_socket(sock))
-    send_data_to_server(sock, "Hello")
+    while True:
+        choice = int(input("1 for get data\n2 for login request\n3 for signup request\n4 to exit"))
+        if choice == 4:
+            break
+        elif choice == 1:
+            print(get_string_data(sock))
+        elif choice == 2:
+            json_data = "{username: \"user1\", password: \"1234\"}"
+            send_request(sock, LOGIN, json_data)
+            print(f"Sent the followong data: {json_data}")
+        elif choice == 3:
+            json_data = "{username: \"user1\", password: \"1234\", mail:\"user1@gmail.com\""
+            send_request(sock, SIGN_UP, json_data)
+            print(f"Sent the followong data: {json_data}")
+        else:
+            print("choose valid option\n")
 
     sock.close()
 
