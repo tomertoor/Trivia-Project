9cfebb42c0b2daef73ea4e2e3d1d43e88f463391
tomertoor <tomertom150@gmail.com>
2022-05-26 11:02:18 +0200

    finished deserializing

--- TriviaProject/TriviaProject/JsonRequestPacketDeserializer.h
+++ TriviaProject/TriviaProject/JsonRequestPacketDeserializer.h
@@ -16,13 +16,15 @@ private:
 	JsonRequestPacketDeserializer() {};
 	
 	static JsonRequestPacketDeserializer* instance;
-
+	static std::string vectorToString(std::vector<unsigned char> vec);
 public:
 
 	static JsonRequestPacketDeserializer* getInstance();
 
 	static Requests::LoginRequest deserializeLoginRequest(Buffer bufer);
 	static Requests::SignupRequest deserializeSignupRequest(Buffer buffer);
-
+	static Requests::GetPlayersInRoomRequest deserializeGetPlayersInRoomRequest(Buffer buffer);
+	static Requests::JoinRoomRequest deserializeJoinRoomRequest(Buffer buffer);
+	static Requests::CreateRoomRequest deserializeCreateRoomRequest(Buffer buffer);
 
 };
