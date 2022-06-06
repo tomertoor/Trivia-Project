44eaef55611b6672c8dfd92bbf7670d31ccebb6c
tomertoor <tomertom150@gmail.com>
2022-06-06 15:38:05 +0300

    Added room design

---
+++ TriviaClient/TriviaClient/Room.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaClient
+{
+    /// <summary>
+    /// Interaction logic for Room.xaml
+    /// </summary>
+    public partial class Room : Window
+    {
+        public Room()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
